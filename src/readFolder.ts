63fd333830d3ef57a3511291952223df7f6ef9ca
eleven <sydryy@gmail.com>
2023-04-05 23:15:59 +0800

    refactor loop

--- src/readFolder.ts
+++ src/readFolder.ts
@@ -2,18 +2,16 @@ import * as fs from 'fs'
 import statistic from './statisticChineseAmount'
 import { OutputProcess } from './outputProcess'
 
-export default async function (path: string, outputHandler: OutputProcess) {
+export default async function processFiles(path: string, outputHandler: OutputProcess) {
 	try {
 		const files = await fs.promises.readdir(path)
-		files.sort() // 对文件名进行排序
-
 		for (const fileName of files) {
 			const fileContent = await fs.promises.readFile(`${path}/${fileName}`)
 			if (fileContent.toString() === '[null]') {
-				await outputHandler.output(fileName, [0, 0])
+				outputHandler.output(fileName, [0, 0])
 				continue
 			}
-			const copyWriting = JSON.parse(fileContent.toString().replace(/[\\ | \s*]/g, ''))
+			const copyWriting = JSON.parse(fileContent.toString().replace(/[\\|\s*]/g, ''))
 			const fileResult = copyWriting.map(sentence => {
 				if (sentence) {
 					const [origin, translation] = sentence
@@ -27,12 +25,9 @@ export default async function (path: string, outputHandler: OutputProcess) {
 				res[1] += currentProcess[1]
 				return res
 			}, [0, 0])
-			await outputHandler.output(fileName, process)
-			if (fileName === files[files.length - 1]) {
-				await new Promise(resolve => setTimeout(resolve, 400))
-				await outputHandler.endStatistic()
-			}
+			outputHandler.output(fileName, process)
 		}
+		outputHandler.endStatistic()
 	} catch (err) {
 		console.error(err)
 	}
