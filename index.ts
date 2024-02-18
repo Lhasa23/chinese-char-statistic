c47405e5ab9dbd647a45725fbe927981ec3d60df
eleven <eleven@xmind.net>
2024-02-18 17:12:19 +0800

    commander to config working path

--- index.ts
+++ index.ts
@@ -1,6 +1,18 @@
 import readFolderByPath from './src/readFolder'
 import { OutputProcess } from './src/outputProcess'
+import { Command } from 'commander'
+const program = new Command()
 
-const outputHandler = new OutputProcess('../WolfACTTranslate/process.json')
-outputHandler.initProcessFile()
-readFolderByPath('../WolfACTTranslate/Json', outputHandler)
+program.version('0.0.1')
+    .name('static-chinese-chars')
+    .usage('<path>')
+    .description('working directory and output directory')
+    .arguments('<path>')
+    .action(path => {
+        console.log(path)
+        const outputHandler = new OutputProcess(`${path}/output/process.json`)
+        outputHandler.initProcessFile()
+        readFolderByPath(path, outputHandler)
+    })
+
+program.parse(process.argv)
